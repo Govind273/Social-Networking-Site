b4bf57e10327377e73eb682e021c3b965bef64e3
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-04 22:13:09 -0400

    Update PostController.java
    updated post list

--- src/main/java/edu/iu/club/connect/controller/PostController.java
+++ src/main/java/edu/iu/club/connect/controller/PostController.java
@@ -9,13 +9,16 @@ import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 
@@ -24,37 +27,40 @@ import org.springframework.web.bind.annotation.SessionAttributes;
  */
 
 @Controller
-@SessionAttributes ({"group_id"})
+@SessionAttributes ({"group_id", "user_id" })
 public class PostController {
 	
     @Autowired
     PostService postService;
 
-    @RequestMapping(value = "/createPost/{group_id}",method = {RequestMethod.GET,RequestMethod.POST})
-    public String createPost( @PathVariable("group_id") int group_id, PostModel postModel){
-    	try{
+    @ResponseBody @RequestMapping(value = "/createPost/{group_id}/{user_id}"  )
+    public String createPost( @PathVariable("group_id") int group_id, @PathVariable("user_id") int user_id,PostModel postModel){
+    	
     	postModel.setGroupId(group_id);
-    	System.out.println("im in");
+    	postModel.setPostedby(user_id);
+    	//System.out.println("im in");
     	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     	Date date = new Date();
     	postModel.setPostedDatetime(date);
     	postService.saveOne(postModel);
+    	List<PostModel> ps= postService.search(postModel);
         return "groupsProfile";
         }
-    	catch(Exception e){
-    		
-    		return "Exception is" +e ;	
-    	}
-    }
+    
+    
+    @RequestMapping(value = "/deletePost/{post_id}/{user_id}",method = RequestMethod.POST)
+    public String deletePost( @PathVariable("post_id") int post_id, @PathVariable("user_id") int user_id){
+//    	postModel.setPostId(post_id);
+    	postService.deleteById(post_id);
+        return "groupsProfile";
+        }
    
-//    public String getDoctorSearchResults(@RequestParam(required=false) 
-    //String LastName,@RequestParam(required=false) String Hospital,
-    //@RequestParam(required=false) String Specialty, @RequestParam(required=false) String date1, ModelMap model) {
-
-//    @RequestMapping(value = "/listPost/",method = RequestMethod.GET)
-//    public String findLatest(PostModel postModel){
-//    	
-//    	return "groupsProfile";
-//    }
+//    @RequestMapping(value = "/groupsProfile/{group_id}",method = RequestMethod.GET)
+//    public String listPost( @PathVariable("group_id") int group_id, PostModel postModel){
+//    	postModel.setGroupId(group_id);
+//    	postService.search(postModel);
+//        return "groupsProfile";
+//        }
+   
 
 }
