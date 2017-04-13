a1e47ca05c53e287df43064edf6674d97e6fb087
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-13 10:01:05 -0400

    Update PostRepository.java
    updated post functionality

--- src/main/java/edu/iu/club/connect/service/repository/PostRepository.java
+++ src/main/java/edu/iu/club/connect/service/repository/PostRepository.java
@@ -2,11 +2,13 @@ package edu.iu.club.connect.service.repository;
 
 import edu.iu.club.connect.model.PostModel;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +26,16 @@ public interface PostRepository extends JpaRepository<PostModel,Integer> {
 //  @Query("select b from PostModel b where LOWER(b.postDesc) LIKE LOWER(CONCAT('%',:postDesc, '%'))")
 //	ArrayList<Object> findPostByName(@Param("postDesc") String postName);
     
-//    @Query("select b from PostModel b where b.group_Id = :group_Id")
-//    List<PostModel> findTop10ByGroupIdAndOrderByLevelDesc(@Param("group_Id") int groupId);
+    @Query(value="select b from PostModel b where b.group_Id = :group_Id order by b.postedDatetime desc")  
+    List<PostModel> findTop10ByGroupIdOrderByPostedDatetimeDesc(@Param("group_Id") int groupId);
+    
+
+    @Query("select b from PostModel b where b.post_Id = :post_Id")
+   PostModel findpostedBy(@Param("post_Id") int post_Id);
+    	
+        
+        
+        
+    	
+    
 }
