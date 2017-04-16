35ea3a983c5e9f17e723b0a524a7b7d1750db802
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-16 16:39:26 -0400

    Update GroupController.java
    Fixed post functionality

--- src/main/java/edu/iu/club/connect/controller/GroupController.java
+++ src/main/java/edu/iu/club/connect/controller/GroupController.java
@@ -2,7 +2,9 @@ package edu.iu.club.connect.controller;
 
 import edu.iu.club.connect.model.GroupMembersModel;
 import edu.iu.club.connect.model.GroupModel;
+import edu.iu.club.connect.model.PostModel;
 import edu.iu.club.connect.service.serviceInterface.GroupService;
+import edu.iu.club.connect.service.serviceInterface.PostService;
 
 import java.util.List;
 
@@ -25,7 +27,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 public class GroupController {
 
-
+	
+	@Autowired
+	PostService postService;
 
 	@Autowired
 	GroupService groupService;
@@ -36,13 +40,20 @@ public class GroupController {
 	}
 	
 	@RequestMapping(value = "/groupPage/{groupId}" , method = RequestMethod.GET)
-	public String openGroupPage(@PathVariable("groupId") int groupId , ModelMap modelMap){
-		
+	public ModelAndView openGroupPage(@PathVariable("groupId") int groupId , ModelMap modelMap, PostModel postModel){
+		ModelAndView mv=new ModelAndView("groupsProfile");
+    	postModel.setGroupId(groupId);
+    	List<PostModel> ps= postService.search(postModel);
+    	if (ps.size()>10){
+    	ps.subList(10,ps.size()).clear();
+    	
+    	}
+    	
+    		mv.addObject("ps",ps); 
 		GroupModel group = groupService.findGroup(groupId);
 		modelMap.put("groupSearched", group);
-		return "groupsProfile";
+		return mv;
 	}
-
 	@RequestMapping(value = "/groupInformation/{admin_id}",method = RequestMethod.POST)
 	public String createGroup(@PathVariable("admin_id") int admin_id , GroupModel groupModel){
 
