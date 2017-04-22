9a7b6254bc21190576197d36dbacf991705c3722
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-22 04:57:10 -0400

    Add files via upload
    job details added

---
+++ src/main/java/edu/iu/club/connect/service/serviceInterface/JobDetailsService.java
@@ -0,0 +1,11 @@
+package edu.iu.club.connect.service.serviceInterface;
+
+import java.util.List;
+
+import edu.iu.club.connect.model.JobDetailsModel;
+
+public interface JobDetailsService {
+
+	public boolean saveOne(JobDetailsModel jobDetailsModel);
+	public List<JobDetailsModel> findAllJobsById(Integer userId);
+}
