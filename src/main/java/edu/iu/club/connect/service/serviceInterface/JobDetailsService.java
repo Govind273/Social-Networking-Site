7dd759423ccc3dafeda9adf956daeda50c8f35e7
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-23 18:38:55 -0400

    Update JobDetailsService.java

--- src/main/java/edu/iu/club/connect/service/serviceInterface/JobDetailsService.java
+++ src/main/java/edu/iu/club/connect/service/serviceInterface/JobDetailsService.java
@@ -8,4 +8,7 @@ public interface JobDetailsService {
 
 	public boolean saveOne(JobDetailsModel jobDetailsModel);
 	public List<JobDetailsModel> findAllJobsById(Integer userId);
+	
+	public void edit(JobDetailsModel jobdetailmodel);
+	public void deleteOne(int jobdetails_Id);
 }
