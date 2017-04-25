9a4c5bbb1f2954891ad2a302b99dadfb859cd184
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-25 05:12:53 -0400

    Update PostModel.java

--- src/main/java/edu/iu/club/connect/model/PostModel.java
+++ src/main/java/edu/iu/club/connect/model/PostModel.java
@@ -17,16 +17,34 @@ public class PostModel {
 	    @SequenceGenerator(name ="postSequence",sequenceName = "posts_post_id_seq",allocationSize = 1)
 	    private @Column(name = "post_id")  Integer post_Id;
 	    private @Column(name = "post_description") String postDesc; //used in search feature
-	    private @Column(name = "posted_by")  Integer postedby;
-
-	    private @Column(name = "group_id")  Integer group_Id;
+	    private @Column(name = "posted_by")  int postedby;
+	    private @Column(name = "postbyname")  String postbyname;
+	    
+		private @Column(name = "group_id")  Integer group_Id;
 	    private @Column(name = "posted_datetime")  Date postedDatetime;
-
+	    public Integer getPost_Id() {
+			return post_Id;
+		}
+		public void setPost_Id(Integer post_Id) {
+			this.post_Id = post_Id;
+		}
+		public String getPostbyname() {
+			return postbyname;
+		}
+		public void setPostbyname(String postbyname) {
+			this.postbyname = postbyname;
+		}
+		public Integer getGroup_Id() {
+			return group_Id;
+		}
+		public void setGroup_Id(Integer group_Id) {
+			this.group_Id = group_Id;
+		}
 	    public PostModel(){}
-	    public Integer getPostedby() {
+	    public int getPostedby() {
 			return postedby;
 		}
-		public void setPostedby(Integer postedby) {
+		public void setPostedby(int postedby) {
 			this.postedby = postedby;
 		}
 	    
