02fa4814d1d8a357ce85b0a5043ec04dff50c1f9
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-27 03:34:25 -0400

    Update GroupServiceImplementation.java

--- src/main/java/edu/iu/club/connect/service/serviceImplementation/GroupServiceImplementation.java
+++ src/main/java/edu/iu/club/connect/service/serviceImplementation/GroupServiceImplementation.java
@@ -73,6 +73,7 @@ public class GroupServiceImplementation implements GroupService {
 	public GroupModel findGroup(int groupId) {
 		
 		GroupModel group = groupRepository.findGroupById(groupId);
+		System.out.println(group.getClubstartdate());
 		return group;
 	}
 
@@ -122,6 +123,19 @@ public class GroupServiceImplementation implements GroupService {
 				);
 		
 		return true;
+	}
+	@Override
+	public List<GroupModel> findMyGroups(int userId) {
+		
+		List<Integer> myGroupids = groupMemberRepository.findgroupsbyUserId(userId);
+		
+		List<GroupModel> myGroups = new ArrayList<GroupModel>();
+		
+		for(Integer groupid : myGroupids){
+			
+			myGroups.add(groupRepository.findGroupById(groupid));
+		}
+		return myGroups;
 	}
 
 }
