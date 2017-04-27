810c7d051ed1acd355cdfb71c8a407854e77a0dc
Vishy91 <vaishnavi.mukundhan@gmail.com>
2017-04-27 02:04:23 -0400

    Update GroupMemberRepository.java
    multiple groups removed

--- src/main/java/edu/iu/club/connect/service/repository/GroupMemberRepository.java
+++ src/main/java/edu/iu/club/connect/service/repository/GroupMemberRepository.java
@@ -17,7 +17,7 @@ public interface GroupMemberRepository extends JpaRepository<GroupMembersModel,I
 	@Query("select b from GroupMembersModel b where b.userId = :userId AND b.groupId = :groupId" )
 	List<GroupMembersModel> isAlreadyJoined(@Param("userId") int userId,@Param("groupId")  int groupId);
 
-	@Query("select b from GroupMembersModel b where b.userId = :userId")
+	@Query("select distinct b from GroupMembersModel b where b.userId = :userId")
 	List<GroupMembersModel> findByUserId(@Param("userId") int userId);
 
 	@Query("select b from GroupMembersModel b where b.groupId = :groupId")
